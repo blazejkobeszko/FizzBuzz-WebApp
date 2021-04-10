03576285bcd431c5dc9cb7a8b77bf419a4125119
Błażej <blazejkob1342@gmail.com>
2021-04-10 21:15:11 +0200

    Test poprawnego utworzenia nowej gałęzi

--- FizzBuzz-WebApp/Startup.cs
+++ FizzBuzz-WebApp/Startup.cs
@@ -23,7 +23,7 @@ namespace FizzBuzz_WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+           // services.AddRazorPages();
             services.AddMemoryCache();
             services.AddSession(options =>
             {
